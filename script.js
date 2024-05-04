d132510c1e95f33cb34113ad292d4bccea7224c5
Siddhi gada <83527318+siddhi-coder@users.noreply.github.com>
2024-05-04 18:11:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const apiurl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
 const weathericon = document.querySelector(".weather-icon");
-const card = document.querySelector(".card");
 async function checkWeather(city){
     const response  =await fetch(apiurl + city + `&appid=${apikey}`);
 
