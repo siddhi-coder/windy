f06aeaa744b11809ff10fdefe442cd0c454fa2bf
siddhi-coder <gadasiddhi092@gmail.com>
2024-05-04 17:58:45 +0530

    commit

--- script.js
+++ script.js
@@ -24,24 +24,25 @@ async function checkWeather(city){
 
     if (data.weather[0].main == "Clouds"){
         weathericon.src = "images/clouds.png";
-        document.card.style.background = "#d3d3d3"; // Light Gray for Clouds
+        card.style.backgroundColor = "#d3d3d3"; // Light Gray for Clouds
     }
     else if (data.weather[0].main == "Clear"){
         weathericon.src = "images/clear.png";
-        document.card.style.background = "#87ceeb"; // Sky Blue for Clear
+        card.style.backgroundColor = "#87ceeb"; // Sky Blue for Clear
     }
     else if (data.weather[0].main == "Rain"){
         weathericon.src = "images/rain.png";
-        document.card.style.background = "#4682b4"; // Steel Blue for Rain
+        card.style.backgroundColor = "#4682b4"; // Steel Blue for Rain
     }
     else if (data.weather[0].main == "Drizzle"){
         weathericon.src = "images/drizzle.png";
-        document.card.style.background = "#a9a9a9"; // Dark Gray for Drizzle
+        card.style.backgroundColor = "#a9a9a9"; // Dark Gray for Drizzle
     }
     else if (data.weather[0].main == "Mist"){
         weathericon.src = "images/mist.png";
-        document.card.style.background = "#d3d3d3"; // Light Gray for Mist (same as Clouds)
+        card.style.backgroundColor = "#d3d3d3"; // Light Gray for Mist (same as Clouds)
     }
+    
     
 
 }
